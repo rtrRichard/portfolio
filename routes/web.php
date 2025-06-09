5368d513a35ad68cf2c2e2172425c5a1f2fc53c9
RJ-Richard-96 <rjrichardparker001@gmail.com>
2025-06-09 17:17:31 +0530

    set up admin controller && Composer for timout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/test', [Admin::class, 'index'])->name('admin.index');
